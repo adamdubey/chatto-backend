72f5dfa73c9db504fc8905654c88c17021c143ac
Adam Dubey <adubey0311@gmail.com>
2022-11-04 10:04:11 -0400

    [AD] Fixup and refactor

--- src/config.ts
+++ src/config.ts
@@ -8,27 +8,37 @@ class Config {
   public DATABASE_URL: string | undefined;
   public JWT_TOKEN: string | undefined;
   public NODE_ENV: string | undefined;
-  public CLIENT_URL: string | undefined;
   public SECRET_KEY_ONE: string | undefined;
   public SECRET_KEY_TWO: string | undefined;
+  public CLIENT_URL: string | undefined;
   public REDIS_HOST: string | undefined;
   public CLOUD_NAME: string | undefined;
   public CLOUD_API_KEY: string | undefined;
   public CLOUD_API_SECRET: string | undefined;
+  public SENDER_EMAIL: string | undefined;
+  public SENDER_EMAIL_PASSWORD: string | undefined;
+  public SENDGRID_API_KEY: string | undefined;
+  public SENDGRID_SENDER: string | undefined;
+  public EC2_URL: string | undefined;
 
-  private readonly DEFAULT_DATABASE_URL = 'mongodb://localhost:27017/chatto-app-db';
+  private readonly DEFAULT_DATABASE_URL = 'mongodb://localhost:27017/chattyapp-backend';
 
   constructor() {
     this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
-    this.JWT_TOKEN = process.env.JWT_TOKEN || 'JWT_TOKEN_123';
+    this.JWT_TOKEN = process.env.JWT_TOKEN || '1234';
     this.NODE_ENV = process.env.NODE_ENV || '';
-    this.CLIENT_URL = process.env.CLIENT_URL || '';
     this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
     this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
+    this.CLIENT_URL = process.env.CLIENT_URL || '';
     this.REDIS_HOST = process.env.REDIS_HOST || '';
     this.CLOUD_NAME = process.env.CLOUD_NAME || '';
     this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
     this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
+    this.SENDER_EMAIL = process.env.SENDER_EMAIL || '';
+    this.SENDER_EMAIL_PASSWORD = process.env.SENDER_EMAIL_PASSWORD || '';
+    this.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '';
+    this.SENDGRID_SENDER = process.env.SENDGRID_SENDER || '';
+    this.EC2_URL = process.env.EC2_URL || '';
   }
 
   public createLogger(name: string): bunyan {
--- src/shared/globals/decorators/joi-validation.decorators.ts
+++ src/shared/globals/decorators/joi-validation.decorators.ts
@@ -12,14 +12,11 @@ export function joiValidation(schema: ObjectSchema): IJoiDecorator {
     descriptor.value = async function (...args: any[]) {
       const req: Request = args[0];
       const { error } = await Promise.resolve(schema.validate(req.body));
-
       if (error?.details) {
         throw new JoiRequestValidationError(error.details[0].message);
       }
-
       return originalMethod.apply(this, args);
     };
-
     return descriptor;
   };
 }
